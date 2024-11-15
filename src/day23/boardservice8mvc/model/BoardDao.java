8e11b0346d3edda50ff887852d80093dab7eac2a
admin <whs001023@gmail.com>
2024-11-15 20:45:35 +0900

    1

--- src/day23/boardservice8mvc/model/BoardDao.java
+++ src/day23/boardservice8mvc/model/BoardDao.java
@@ -1,20 +1,29 @@
 package day23.boardservice8mvc.model;
 
+import day23.Board;
+
 import java.util.ArrayList;
 
-public class BoardDao {
+public class BoardDao{
+
+    //싱글톤
+    private static final BoardDao boardDao = new BoardDao();
+    private BoardDao(){}
+    public static BoardDao getInstance(){
+        return boardDao;
+    }
 
     ArrayList<BoardDto> boardDB = new ArrayList<>();
 
     // 1. 게시물 등록 접근 함수
-    public boolean boardWrite( BoardDto boardDto){
-        boardDB.add( boardDto );
+    public boolean boardWrite(BoardDto boardDto){
+        boardDB.add(boardDto);
         return true;
     }
 
     // 2. 게시물 출력 접근 함수
-    public void boardPrint( ){
-
+    public ArrayList<BoardDto> boardPrint( ){
+        return boardDB;
     }
 
 }
